427787b9ec91a839e53bd0804c14ec8f89df5d5f
waffe_x20 <argueta_eduardo@yahoo.de>
2022-01-10 21:25:46 -0600

    add CORS

--- core/settings.py
+++ core/settings.py
@@ -39,12 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'blog_api',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -132,3 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
         'rest_framework.permissions.IsAuthenticated',
     ]
 } """
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]

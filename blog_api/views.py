c37f75b0775c17827c798d40f9cae968f57368f6
waffe_x20 <argueta_eduardo@yahoo.de>
2022-01-13 21:29:50 -0600

    Add update/delete to Detail view

--- blog_api/views.py
+++ blog_api/views.py
@@ -8,6 +8,6 @@ class PostList(generics.ListCreateAPIView):
     queryset = Post.postobjects.all()
     serializer_class = PostSerializer
 
-class PostDetail(generics.RetrieveAPIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
